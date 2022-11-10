78ffff66de2a2fcca0691afd64dcb69e8ef389e2
alinamkhoyan00 <alinamkhoyan19@gmail.com>
2022-11-10 14:38:29 +0400

    JDBC assignments

--- src/main/java/jdbc/CustomConnector.java
+++ src/main/java/jdbc/CustomConnector.java
@@ -6,24 +6,28 @@ import java.sql.SQLException;
 
 
 public class CustomConnector {
+    private static final String DRIVER_CLASS_NAME = "org.postgresql.Driver";
+    public Connection connection;
 
     public Connection getConnection(String url) {
 
         try {
-            return DriverManager.getConnection(url);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+            Class.forName(DRIVER_CLASS_NAME);
+            connection = DriverManager.getConnection(url);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
         }
-
+        return connection;
     }
 
-    public Connection getConnection(String url, String user, String password) {
-
+    public Connection getConnection(String url, String user, String password){
         try {
-            return DriverManager.getConnection(url, user, password);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+            CustomDataSource customDataSource = CustomDataSource.getInstance();
+            connection = customDataSource.getConnection(url, user, password);
+        }catch (Exception e){
+            e.printStackTrace();
         }
 
+        return connection;
     }
 }
